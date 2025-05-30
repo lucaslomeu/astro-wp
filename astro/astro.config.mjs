796b7b69f2f951fdb04942f6aa605d76a3f1beef
Lucas Lomeu <lucaslomeugomes@gmail.com>
2025-05-30 16:29:13 -0300

    [FIX] WP

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercelAdapter from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
 
-    adapter: vercelAdapter(),
 
     // Configuração para rotas dinâmicas
     build: {
